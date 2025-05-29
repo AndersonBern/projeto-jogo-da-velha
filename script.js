d8f7171c0651d7f27b115c9ef40f3f2ce0146acc
Anderson D Bernardo <anderson.danttas@hotmail.com>
2025-05-29 18:14:59 -0300

    Project finished

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function escolherCampo(id) {
 
         campo.classList.add('preenchido');
 
-        campo.style.backgroundImage = `url(imagens/img-${player}.png)`
+        campo.style.backgroundImage = `url(imagens/img-${player}.png)`;
 
         
     }
@@ -41,8 +41,15 @@ let listaJogadaPC = [jogada];
 let listaJogadaPlayer = [];
 
 function sorteioPC() {
-    jogada = Math.floor(Math.random() * 9) + 1;
-    console.log(jogada);
+
+    if (listaJogadaPC.length < 5) {
+        
+        jogada = Math.floor(Math.random() * 9) + 1;
+        console.log(jogada);
+    }
+    else {
+        return;
+    }
 }
 
 
@@ -73,6 +80,8 @@ function jogadaPC() {
 
             campoPc.style.backgroundImage = `url(imagens/img-${cpu}.png)`
         }
+
+        let delay = setInterval(resultado, 1000)
         
     }
         
@@ -80,6 +89,136 @@ function jogadaPC() {
     
 }
 
-function vencedor() {
+let vencedor = document.getElementById('vencedor');
+let corDeFundo = document.getElementById('fundo');
+let mensagem = document.getElementById('msg');
 
+function resultado() {
+
+        //PARA O JOGADOR.
+
+    //Horizontal linha 1
+    if((listaJogadaPlayer.indexOf(1) != -1) && (listaJogadaPlayer.indexOf(2) != -1) && (listaJogadaPlayer.indexOf(3) != -1)){
+
+        corDeFundo.style.display = 'block';
+        vencedor.style.display = 'block';
+        mensagem.innerHTML = 'Você GANHOU!';
+        
+    }
+    //Horizontal linha 2
+    else if((listaJogadaPlayer.indexOf(4) != -1) && (listaJogadaPlayer.indexOf(5) != -1) && (listaJogadaPlayer.indexOf(6) != -1)){
+    
+        corDeFundo.style.display = 'block';
+        vencedor.style.display = 'block'
+        mensagem.innerHTML = 'Você GANHOU!'
+    }
+    //Horizontal linha 3
+    else if((listaJogadaPlayer.indexOf(7) != -1) && (listaJogadaPlayer.indexOf(8) != -1) && (listaJogadaPlayer.indexOf(9) != -1)){
+
+        corDeFundo.style.display = 'block';
+        vencedor.style.display = 'block'
+        mensagem.innerHTML = 'Você GANHOU!'
+    }
+    //Vertical linha 1
+    else if((listaJogadaPlayer.indexOf(1) != -1) && (listaJogadaPlayer.indexOf(4) != -1) && (listaJogadaPlayer.indexOf(7) != -1)){
+
+        corDeFundo.style.display = 'block';
+        vencedor.style.display = 'block'
+        mensagem.innerHTML = 'Você GANHOU!'
+    }
+    //Vertical linha 2
+    else if((listaJogadaPlayer.indexOf(2) != -1) && (listaJogadaPlayer.indexOf(5) != -1) && (listaJogadaPlayer.indexOf(8) != -1)){
+
+        corDeFundo.style.display = 'block';
+        vencedor.style.display = 'block'
+        mensagem.innerHTML = 'Você GANHOU!'
+    }
+    //Vertical linha 3
+    else if((listaJogadaPlayer.indexOf(3) != -1) && (listaJogadaPlayer.indexOf(6) != -1) && (listaJogadaPlayer.indexOf(9) != -1)){
+
+        corDeFundo.style.display = 'block';
+        vencedor.style.display = 'block'
+        mensagem.innerHTML = 'Você GANHOU!'
+    }
+    //Diagonal inicio linha 1
+    else if((listaJogadaPlayer.indexOf(1) != -1) && (listaJogadaPlayer.indexOf(5) != -1) && (listaJogadaPlayer.indexOf(9) != -1)){
+
+        corDeFundo.style.display = 'block';
+        vencedor.style.display = 'block'
+        mensagem.innerHTML = 'Você GANHOU!'
+    }
+    //Diagonal inicio linha 3
+    else if((listaJogadaPlayer.indexOf(3) != -1) && (listaJogadaPlayer.indexOf(5) != -1) && (listaJogadaPlayer.indexOf(7) != -1)){
+
+        corDeFundo.style.display = 'block';
+        vencedor.style.display = 'block'
+        mensagem.innerHTML = 'Você GANHOU!'
+    }
+
+        //PARA O CPU
+
+    //Horizontal linha 1
+    if((listaJogadaPC.indexOf(1) != -1) && (listaJogadaPC.indexOf(2) != -1) && (listaJogadaPC.indexOf(3) != -1)){
+
+        corDeFundo.style.display = 'block';
+        corDeFundo.style.backgroundColor = '#f54d4d91'
+        vencedor.style.display = 'block'
+        mensagem.innerHTML = 'Você PERDEU!'
+    }
+    //Horizontal linha 2
+    else if((listaJogadaPC.indexOf(4) != -1) && (listaJogadaPC.indexOf(5) != -1) && (listaJogadaPC.indexOf(6) != -1)){
+
+        corDeFundo.style.display = 'block';
+        corDeFundo.style.backgroundColor = '#f54d4d91'
+        vencedor.style.display = 'block'
+        mensagem.innerHTML = 'Você PERDEU!'
+    }
+    //Horizontal linha 3
+    else if((listaJogadaPC.indexOf(7) != -1) && (listaJogadaPC.indexOf(8) != -1) && (listaJogadaPC.indexOf(9) != -1)){
+
+        corDeFundo.style.display = 'block';
+        corDeFundo.style.backgroundColor = '#f54d4d91'
+        vencedor.style.display = 'block'
+        mensagem.innerHTML = 'Você PERDEU!'
+    }
+    //Vertical linha 1
+    else if((listaJogadaPC.indexOf(1) != -1) && (listaJogadaPC.indexOf(4) != -1) && (listaJogadaPC.indexOf(7) != -1)){
+
+        corDeFundo.style.display = 'block';
+        corDeFundo.style.backgroundColor = '#f54d4d91'
+        vencedor.style.display = 'block'
+        mensagem.innerHTML = 'Você PERDEU!'
+    }
+    //Vertical linha 2
+    else if((listaJogadaPC.indexOf(2) != -1) && (listaJogadaPC.indexOf(5) != -1) && (listaJogadaPC.indexOf(8) != -1)){
+
+        corDeFundo.style.display = 'block';
+        corDeFundo.style.backgroundColor = '#f54d4d91'
+        vencedor.style.display = 'block'
+        mensagem.innerHTML = 'Você PERDEU!'
+    }
+    //Vertical linha 3
+    else if((listaJogadaPC.indexOf(3) != -1) && (listaJogadaPC.indexOf(6) != -1) && (listaJogadaPC.indexOf(9) != -1)){
+
+        corDeFundo.style.display = 'block';
+        corDeFundo.style.backgroundColor = '#f54d4d91'
+        vencedor.style.display = 'block'
+        mensagem.innerHTML = 'Você PERDEU!'
+    }
+    //Diagonal inicio linha 1
+    else if((listaJogadaPC.indexOf(1) != -1) && (listaJogadaPC.indexOf(5) != -1) && (listaJogadaPC.indexOf(9) != -1)){
+
+        corDeFundo.style.display = 'block';
+        corDeFundo.style.backgroundColor = '#f54d4d91'
+        vencedor.style.display = 'block'
+        mensagem.innerHTML = 'Você PERDEU!'
+    }
+    //Diagonal inicio linha 3
+    else if((listaJogadaPC.indexOf(3) != -1) && (listaJogadaPC.indexOf(5) != -1) && (listaJogadaPC.indexOf(7) != -1)){
+
+        corDeFundo.style.display = 'block';
+        corDeFundo.style.backgroundColor = '#f54d4d91'
+        vencedor.style.display = 'block'
+        mensagem.innerHTML = 'Você PERDEU!'
+    }
 }
